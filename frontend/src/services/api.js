c9c72802db8e41455ca8400386b6613c79628c8c
pavankumarkaniki <kpavankumar971@gmail.com>
2025-12-13 21:10:59 +0530

    Frontend checking

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://community-board-backend-bde1.onrender.com/",
 });
 
 API.interceptors.request.use((config) => {
